5dae283bc73f594b696518ff67fe7deea96197a3
Cristian Daniel Colocho López <colochocristian@outlook.com>
2025-03-24 11:04:01 -0600

    arreglando vistas

--- CDCL20250324.AppWebMVC/Models/Product.cs
+++ CDCL20250324.AppWebMVC/Models/Product.cs
@@ -23,7 +23,7 @@ public partial class Product
     public int? WarehouseId { get; set; }
 
     public int? BrandId { get; set; }
-
+    [Display(Name = "Notas")]
     public string? Notes { get; set; }
 
     public virtual Brand? Brand { get; set; }
--- CDCL20250324.AppWebMVC/Models/Warehouse.cs
+++ CDCL20250324.AppWebMVC/Models/Warehouse.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CDCL20250324.AppWebMVC.Models;
 
 public partial class Warehouse
 {
     public int Id { get; set; }
-
+    [Required(ErrorMessage = "El nombre de la bodega es obligatorio.")]
+    [Display(Name = "Bodega")]
     public string WarehouseName { get; set; } = null!;
-
+    [Required(ErrorMessage = "Debe insertar una nota.")]
+    [Display(Name = "Notas")]
     public string? Notes { get; set; }
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
